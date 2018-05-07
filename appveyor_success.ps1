f4ef3904e03756ab79fe84d4eb788a362823e6e4
Andrew Pearce <andrew@austoonz.net>
2018-05-06 23:17:13 -0700

    Further bug fixes

--- appveyor_success.ps1
+++ appveyor_success.ps1
@@ -1,12 +1,15 @@
 Get-ChildItem .\Archive | ForEach-Object { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
 
 "Branch: $($env:APPVEYOR_REPO_BRANCH)"
-if ($env:APPVEYOR_REPO_BRANCH -eq 'master') {
+if ($env:APPVEYOR_REPO_BRANCH -eq 'master')
+{
+    'Setting git credentials'
     git config --global credential.helper store
     Add-Content "$HOME\.git-credentials" "https://$($env:GitHubKey):x-oauth-basic@github.com`n"
     git config --global user.name "Andrew Pearce"
     git config --global user.email "andrew@austoonz.net"
-    git checkout master
+
+    'Adding new Module Manifest'
     Copy-Item -Path .\Artifact\Convert.psd1 -Destination .\Convert\Convert.psd1
     git add .\Convert\Convert.psd1
     git commit -m "Module Manifest updated (skip ci)"
