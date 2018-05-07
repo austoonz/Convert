c4d785fe59ca99fc74a622be208b1578b6b624d1
Andrew Pearce <andrew@austoonz.net>
2018-05-06 22:36:56 -0700

    Updated Build

--- Convert.build.ps1
+++ Convert.build.ps1
@@ -19,7 +19,7 @@ Enter-Build {
     $script:UnitTestsPath = Join-Path -Path $script:TestsPath -ChildPath 'Unit'
     $script:PesterTestResultsFile = Join-Path -Path $BuildRoot -ChildPath 'TestsResults.xml'
 
-    $script:ArtifactsPath = Join-Path -Path $BuildRoot -ChildPath 'Artifacts'
+    $script:ArtifactsPath = Join-Path -Path $BuildRoot -ChildPath 'Artifact'
     $script:ArchivePath = Join-Path -Path $BuildRoot -ChildPath 'Archive'
 }
 
