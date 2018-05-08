e4d6141848c4db9d58cc6238aa0a72915a8d5b51
Andrew Pearce <andrew@austoonz.net>
2018-05-07 22:27:27 -0700

    Added help and code formatting.

--- Convert/Public/ConvertFrom-Base64ToString.ps1
+++ Convert/Public/ConvertFrom-Base64ToString.ps1
@@ -1,16 +1,58 @@
+<#
+    .SYNOPSIS
+        Converts a base64 encoded string to a string.
+    
+    .DESCRIPTION
+        Converts a base64 encoded string to a string.
+    
+    .PARAMETER String
+        A Base64 Encoded String
+    
+    .PARAMETER Encoding
+        The encoding to use for conversion.
+        Defaults to UTF8.
+        Valid options are ASCII, BigEndianUnicode, Default, Unicode, UTF32, UTF7, and UTF8.
+    
+    .EXAMPLE
+        PS C:\> ConvertFrom-Base64ToString -String 'QSBzdHJpbmc='
+        A string
+    
+    .EXAMPLE
+        PS C:\> ConvertTo-Base64 -String 'A string','Another string'
+        QSBzdHJpbmc=
+        QW5vdGhlciBzdHJpbmc=
+    
+    .EXAMPLE
+        PS C:\> 'QSBzdHJpbmc=' | ConvertFrom-Base64ToString
+        A string
+
+    .EXAMPLE
+        PS C:\> 'QSBzdHJpbmc=','QW5vdGhlciBzdHJpbmc=' | ConvertFrom-Base64ToString
+        A string
+        Another string
+    
+    .EXAMPLE
+        PS C:\> ConvertTo-Base64 -String 'A string' -Encoding Unicode
+        QQAgAHMAdAByAGkAbgBnAA==
+        
+    .OUTPUTS
+        String
+#>
 function ConvertFrom-Base64ToString
 {
     [CmdletBinding()]
+    [OutputType('String')]
     param
     (
-        [Parameter(Mandatory=$true,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true)]
         [ValidateNotNullOrEmpty()]
-        [String[]]
-        $String,
+        [String[]] $String,
 
-        [ValidateSet('ASCII','BigEndianUnicode','Default','Unicode','UTF32','UTF7','UTF8')]
-        [String]
-        $Encoding = 'UTF8'
+        [ValidateSet('ASCII', 'BigEndianUnicode', 'Default', 'Unicode', 'UTF32', 'UTF7', 'UTF8')]
+        [String] $Encoding = 'UTF8'
     )
     
     Begin
