c026bf9adc7e296a91e05db06b2a6756645fcdff
Andrew Pearce <andrew@austoonz.net>
2020-05-25 23:01:12 -0700

    More AppVeyor fixed

--- appveyor_prepare.ps1
+++
@@ -1,7 +0,0 @@
-# This is a fix for the Visual Studio 2019 AppVeyor image.
-# This image runs PowerShell 7 and includes the AWSPowerShell module by default.
-# This module is not supported on PowerShell 7.
-$path = (Get-Module -ListAvailable -Name 'AWSPowerShell' -ErrorAction 'SilentlyContinue').Path
-if ($path) {
-    Remove-Item -Path $path -Force -Recurse
-}
